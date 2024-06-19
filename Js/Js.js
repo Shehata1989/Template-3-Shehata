1739c0075a978d94a5e52df97feea9bc0bf1afba
Ahmed Shehata <113997581+Shehata1989@users.noreply.github.com>
2024-06-19 22:42:18 +0300

    Add files via upload

--- Js/Js.js
+++ Js/Js.js
@@ -1,8 +1,68 @@
 const btn = document.querySelector("#btn-mega");
 const mega = document.querySelector(".mega-menu");
+const imgMegaMenu = document.querySelector('[alt="megamenu"]');
+const mainNav = document.querySelector(".main-nav");
 
-console.log(mega);
+btn.addEventListener("click", megaActive);
 
-btn.addEventListener("click", () => {
+let overlay = document.createElement("div");
+overlay.className = "overlay";
+
+function megaActive() {
   mega.classList.toggle("active");
+  mainNav.append(overlay);
+}
+
+imgMegaMenu.addEventListener("click", (e) => e.stopPropagation());
+
+document.addEventListener("click", function (e) {
+  e.stopPropagation();
+  if (e.target !== mega && e.target !== btn) {
+    mega.classList.remove("active");
+    if (!mega.classList.contains("active")) {
+      overlay.remove();
+    }
+  }
 });
+
+// #####################################################
+
+const secSkills = document.querySelector(".our-skills"),
+  progress = document.querySelectorAll(".our-skills .the-progress > span");
+
+window.addEventListener("scroll", function () {
+  if (scrollY >= secSkills.offsetTop - 100) {
+    progress.forEach((span) => {
+      span.style.width = span.dataset.attr;
+    });
+    progress.forEach((span) => {
+      span.classList.add("active");
+    });
+  }
+});
+
+// #####################################################
+
+const secStats = document.querySelector(".stats"),
+  spanCounter = document.querySelectorAll(".stats span.counter");
+let start = false;
+
+window.addEventListener("scroll", function () {
+  if (scrollY >= secStats.offsetTop - 100) {
+    if (!start) {
+      spanCounter.forEach((span) => startCount(span))
+      start = true;
+    }
+  }
+});
+
+function startCount (num) {
+  let count = setInterval(function () {
+    num.textContent++;
+    if (num.textContent === num.dataset.count) {
+      clearInterval(count);
+    }
+  }, 1000 / num.dataset.count);
+}
+
+// #####################################################
